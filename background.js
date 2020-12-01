ba93a4a91a1f4a9a4e79721667766b7c2baa4b8a
Ryan <ryanker1024@gmail.com>
2020-12-01 17:03:00 +0800

    修改 ing 为 读

--- background.js
+++ background.js
@@ -123,7 +123,7 @@ function speak(text) {
             if (k === 0) {
                 chrome.tts.isSpeaking(function (speaking) {
                     if (speaking) stop()
-                    chrome.browserAction.setBadgeText({text: 'ing'})
+                    chrome.browserAction.setBadgeText({text: '读'})
                     chrome.browserAction.setBadgeBackgroundColor({color: 'red'})
                     chrome.tts.speak(v, options)
                 })
